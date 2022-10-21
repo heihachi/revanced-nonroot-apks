24a41e6a4c013efc373ba53798cbbc1565435bec
E85Addict <77761710+E85Addict@users.noreply.github.com>
2022-10-21 13:30:26 -0700

    Sync with Upstream

--- build.sh
+++ build.sh
@@ -33,43 +33,37 @@ if [ "$UPDATE_PREBUILTS" = true ]; then get_prebuilts; else set_prebuilts; fi
 reset_template
 get_cmpr
 
-if [ "$BUILD_TWITTER" = true ]; then build_twitter; fi
-if [ "$BUILD_REDDIT" = true ]; then build_reddit; fi
-if [ "$BUILD_WARN_WETTER" = true ]; then build_warn_wetter; fi
-if [ "$BUILD_TIKTOK" = true ]; then build_tiktok; fi
-if [ "$BUILD_SPOTIFY" = true ]; then build_spotify; fi
-if [ "$BUILD_YT" = true ]; then build_yt; fi
-if [ "$BUILD_MUSIC_ARM64_V8A" = true ]; then build_music $ARM64_V8A; fi
-if [ "$BUILD_MUSIC_ARM_V7A" = true ]; then build_music $ARM_V7A; fi
+if ((COMPRESSION_LEVEL > 9)) || ((COMPRESSION_LEVEL < 1)); then
+	abort "COMPRESSION_LEVEL must be between 1 and 9"
+fi
+
+build_youtube
+build_music $ARM64_V8A
+build_music $ARM_V7A
+build_twitter
+build_reddit
+build_tiktok
+build_spotify
+build_warn_wetter
+
 if [ "$BUILD_MINDETACH_MODULE" = true ]; then
 	echo "Building mindetach module"
 	cd mindetach-magisk/mindetach/
 	: >detach.txt
-	if [[ ${YT_PATCHER_ARGS} = *-e\ ?(music-)microg-support* ]]; then
-		echo "com.google.android.youtube" >>detach.txt
-	fi
-	if [[ ${MUSIC_PATCHER_ARGS} = *-e\ ?(music-)microg-support* ]]; then
+	if [ "${YOUTUBE_MODE%/*}" = module ]; then echo "com.google.android.youtube" >>detach.txt; fi
+	if [ "${TWITTER_MODE%/*}" = module ]; then echo "com.twitter.android" >>detach.txt; fi
+	if [ "${REDDIT_MODE%/*}" = module ]; then echo "com.reddit.frontpage" >>detach.txt; fi
+	if [ "${TIKTOK_MODE%/*}" = module ]; then echo "com.zhiliaoapp.musically" >>detach.txt; fi
+	if [ "${SPOTIFY_MODE%/*}" = module ]; then echo "com.spotify.music" >>detach.txt; fi
+	if [ "${WARN_WETTER_MODE%/*}" = module ]; then echo "de.dwd.warnapp" >>detach.txt; fi
+	if [ "${MUSIC_ARM64_V8A_MODE%/*}" = module ] || [ "${MUSIC_ARM_V7A_MODE%/*}" = module ]; then
 		echo "com.google.android.apps.youtube.music" >>detach.txt
-	fi
-	if [ "$TWITTER_MODULE" = true ]; then
-		echo "com.twitter.android" >>detach.txt
-	fi
-	if [ "$REDDIT_MODULE" = true ]; then
-		echo "com.reddit.frontpage" >>detach.txt
-	fi
-	if [ "$WARNWETTER_MODULE" = true ]; then
-		echo "de.dwd.warnapp" >>detach.txt
-	fi
-	if [ "$TIKTOK_MODULE" = true ]; then
-		echo "com.zhiliaoapp.musically" >>detach.txt
-	fi	
-	if [ "$SPOTIFY_MODULE" = true ]; then
-		echo "com.spotify.music" >>detach.txt
 	fi
 	zip -r ../../build/mindetach.zip .
 	cd ../../
 fi
-log "\n[revanced-magisk-module-repo](https://github.com/E85Addict/revanced-magisk-module-repo)"
+
+log "\n[revanced-magisk-module-repo](https://github.com/E85Addict/revanced-magisk-module)"
 
 reset_template
 echo "Done"
