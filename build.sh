405fd0fa41fbce4282e94a144d2a6cd2ac3b60eb
E85Addict <77761710+E85Addict@users.noreply.github.com>
2022-11-03 15:48:41 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -66,7 +66,7 @@ if [ "$BUILD_MINDETACH_MODULE" = true ]; then
 fi
 
 if [[ "${YOUTUBE_MODE%/*}" =~ ^(apk|both)$ || "${MUSIC_ARM64_V8A_MODE%/*}" =~ ^(apk|both)$ || "${MUSIC_ARM_V7A_MODE%/*}" =~ ^(apk|both)$ ]]; then
-	log "\nInstall [Vanced Microg](https://github.com/TeamVanced/VancedMicroG/releases) to be able to use non-root YouTube or Music"
+	log "\nInstall [Vanced Microg](https://github.com/inotia00/VancedMicroG/releases) to be able to use non-root YouTube or Music"
 fi
 log "\n[revanced-magisk-module-repo](https://github.com/E85Addict/revanced-magisk-module)"
 
